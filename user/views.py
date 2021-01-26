6e39d3d8bf8c64f90086192b39bbb74f2ccfd77b
mahidulmoon <mahidulmoon@gmail.com>
2021-01-26 22:05:01 +0600

    pagination added

--- user/views.py
+++ user/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import UserProfileSerializer
 from .models import UserProfile
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .pagination import ListPagination
 # Create your views here.
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    pagination_class = ListPagination
+
