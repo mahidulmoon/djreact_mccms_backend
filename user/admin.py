a324a5e3a7630a4a626400f89e1c19167df60354
mahidulmoon <mahidulmoon@gmail.com>
2021-01-26 21:08:47 +0600

    userprofile model added

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+class UserProfileShow(admin.ModelAdmin):
+    list_display = ("user","phone","address")
+
+admin.site.register(UserProfile,UserProfileShow)
