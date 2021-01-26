4537d13e31bd4602e96d555566e600faab71bc1a
mahidulmoon <mahidulmoon@gmail.com>
2021-01-27 01:30:06 +0600

    user registration api works

--- user/models.py
+++ user/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='user')
     phone = models.CharField(max_length=14)
     address = models.TextField()
--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserProfile
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer(required=True)
     class Meta:
         model = UserProfile
-        fields = "__all__"
+        fields = ('user','phone','address')
+    
+    def create(self,validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(),validated_data=user_data)
+        Token.objects.create(user=user)
+        UserProfile.objects.create(user=user,phone=validated_data.pop('phone'),address=validated_data.pop('address'))
+        return user
