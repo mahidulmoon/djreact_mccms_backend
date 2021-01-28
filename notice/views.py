43f12c84d896c21e7bad4ad7fb353aab7ad6161e
mahidulmoon <mahidulmoon@gmail.com>
2021-01-28 13:23:49 +0600

    pagination added in notice

--- notice/views.py
+++ notice/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Notice
 from .serializers import NoticeSerializer
 from rest_framework import viewsets
+from .pagination import LimitofPage
 # Create your views here.
 
 
 class NoticeViewSet(viewsets.ModelViewSet):
     queryset = Notice.objects.all().order_by('-created_at')
     serializer_class = NoticeSerializer
+    pagination_class = LimitofPage
