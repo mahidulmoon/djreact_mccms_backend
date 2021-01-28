ee6f1a1cd417df6a97f8ee3cc06da6e5cf7e9e3d
mahidulmoon <mahidulmoon@gmail.com>
2021-01-28 19:44:09 +0600

    disable viewset post

--- complain/views.py
+++ complain/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from .models import Complain_table
 from .serializers import ComplainSerializer
 from .pagination import NumberOfComplain
+from rest_framework.response import Response
 # Create your views here.
 
 
@@ -10,3 +11,5 @@ class ComplainViewSet(viewsets.ModelViewSet):
     queryset = Complain_table.objects.all().order_by('-created_at')
     serializer_class = ComplainSerializer
     pagination_class = NumberOfComplain
+    def create(self, request):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
