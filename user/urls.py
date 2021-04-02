2a613fab5d23ccbe8178b602c01f42a736153251
mahidulmoon <mahidulmoon@gmail.com>
2021-04-02 16:40:37 +0600

    api for admin dashboard short

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework import routers
-from .views import UserProfileViewSet,PreviligedUser,CustomObtainAuthToken
+from .views import UserProfileViewSet,PreviligedUser,CustomObtainAuthToken,adminDashShort
 
 
 router = routers.DefaultRouter()
@@ -11,4 +11,5 @@ router.register('previligeduser',PreviligedUser)
 urlpatterns = [
     path('',include(router.urls)),
     path('login',CustomObtainAuthToken.as_view()),
+    path('admindashshort',adminDashShort),
 ]
--- user/views.py
+++ user/views.py
@@ -3,13 +3,42 @@ from rest_framework import viewsets
 from .serializers import UserProfileSerializer,UserSerializer
 from .models import UserProfile
 from django.contrib.auth.models import User
-from rest_framework.decorators import action
+from rest_framework.decorators import action,api_view
 from rest_framework.response import Response
 from .pagination import ListPagination
 from rest_framework.filters import SearchFilter
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from complain.models import Complain_table
+from notice.models import Notice
+from django.contrib.auth.models import User
 # Create your views here.
+
+@api_view(['GET'])
+def adminDashShort(requst):
+
+    all_complains = Complain_table.objects.all().count()
+    pending_complains = Complain_table.objects.filter(status="pending request").count()
+    approve_complains = Complain_table.objects.filter(status="approved").count()
+    solved_complains = Complain_table.objects.filter(status="solved").count()
+    notices = Complain_table.objects.all().count()
+    normal_user = User.objects.all().count()
+    prev_user = User.objects.filter(is_staff=True).count()
+    admin_user = User.objects.filter(is_superuser=True).count()
+    if requst.method == 'GET':
+        return Response({
+            "complains":all_complains,
+            "pendin_complains" : pending_complains,
+            "approve_complains" : approve_complains,
+            "solved_complains" : solved_complains,
+            "notices" : notices,
+            "normal_user" : normal_user,
+            "prev_user" : prev_user,
+            "admin_user" : admin_user
+        })
+
+        
+
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
