bbb0cab823aafc766340a56e532d50db77b71704
mahidulmoon <mahidulmoon@gmail.com>
2021-05-31 01:26:34 +0600

    api working on otp send for dynamic number

--- user/views.py
+++ user/views.py
@@ -21,18 +21,21 @@ from twilio.rest import Client
 def phoneOTP(request):
     if request.method == 'POST':
         gen_otp = random.randint(1000,9999)
-        phone = request.POST.get('phone')
+        #phone = request.POST.get('phone')
         # account_sid = os.environ['AC614ea6e599d1f201e132b1e7b9e45fa2']
         # auth_token = os.environ['cbbcd8d1f8b4f472f2b46c8ff4ad2392']
+        phone = request.data['phone']
         account_sid = 'AC614ea6e599d1f201e132b1e7b9e45fa2'
         auth_token = 'cbbcd8d1f8b4f472f2b46c8ff4ad2392'
         client = Client(account_sid, auth_token)
+        #print('+88'+str(phone))
 
         message = client.messages.create(
-                body='Your OTP is  '+str(gen_otp)+' this will destroyed after 3 min. Thank you',
+                body='Your OTP is  '+str(gen_otp)+' this will be destroyed after 3 min. Thank you for registering into Municipal Corporation Complain Management System',
                 from_='+17742373088',
                 # to=str(phone)
-                to = '+8801771042196'
+                # to = '+8801771042196'
+                to = '+88'+str(phone)
             )
         return Response({'otp': gen_otp,'message' : 'otp send to '+str(phone)})
 
